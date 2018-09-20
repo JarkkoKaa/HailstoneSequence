770b6f3e0cc92c414c75767a775830214e3e379b
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2018-09-20 19:09:11 +0300

    Add project files.

---
+++ HailstoneSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HailstoneSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            Console.WriteLine(n);
+            do
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                    Console.WriteLine(n);
+                }             
+                else
+                {
+                    n *= 3;
+                    n++;
+                    Console.WriteLine(n);
+                }
+            } while (n != 1);
+        }
+    }
+}
