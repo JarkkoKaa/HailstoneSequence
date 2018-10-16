20a748fe2e9866ff08e6ca8ef63d84e47b984875
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2018-10-16 21:53:18 +0300

    Iteration and Rescursion added

--- HailstoneSequence/Program.cs
+++ HailstoneSequence/Program.cs
@@ -6,22 +6,14 @@ namespace HailstoneSequence
     {
         static void Main(string[] args)
         {
-            int n = 9;
-            Console.WriteLine(n);
-            do
-            {
-                if (n % 2 == 0)
-                {
-                    n /= 2;
-                    Console.WriteLine(n);
-                }             
-                else
-                {
-                    n *= 3;
-                    n++;
-                    Console.WriteLine(n);
-                }
-            } while (n != 1);
+            int n = 3;
+            Console.WriteLine("Recursion:");
+            SequenceRecursion sr = new SequenceRecursion();
+            sr.HStoneRecursion(n);
+            Console.WriteLine("\nIteration:");
+            SequenceIteration si = new SequenceIteration();
+            si.HStoneIteration(n);
+            
         }
     }
 }
---
+++ HailstoneSequence/SequenceIteration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HailstoneSequence
+{
+    class SequenceIteration
+    {
+        public void HStoneIteration(int n)
+        {
+            Console.WriteLine(n);
+            do
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    n *= 3;
+                    n++;
+                    Console.WriteLine(n);
+                }
+            } while (n != 1);
+        }  
+    }
+}
